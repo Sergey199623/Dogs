a1a7315a7996e469a74ad2ddde8827ba1974bb83
Sergey <belyakovsergeyrus@yandex.ru>
2020-05-28 11:57:42 +0300

    Update

--- src/Dogs.kt
+++ src/Dogs.kt
@@ -13,15 +13,35 @@ class Dog(val name: String,
         println("The breed is $breed")
     }
 
-    var weigh = weight_param
+    var weight = weight_param
         set (value) {
             if (value > 0) field = value
         }
 
     val weightInKgs: Double
-        get() = weigh / 2.2
+        get() = weight / 2.2
 
     fun bark() {
-        println(if (weigh < 20) "Yip!" else "Woof!")
+        println(if (weight < 20) "Yip!" else "Woof!")
     }
+}
+
+fun main(args: Array<String>) {
+    val myDog = Dog("Fido", 70, "Mixed")
+    myDog.bark()
+    myDog.weight = 75
+    println("Weight in Kgs is ${myDog.weightInKgs}")
+    myDog.weight = -2
+    println("Weight is ${myDog.weight}")
+    myDog.activities = arrayOf("Walks", "Fetching balls", "Frisbee")
+    for (item in myDog.activities) {
+        println("My dog enjoys $item")
+    }
+
+    val dogs = arrayOf(Dog("Kelpie", 20, "Westie"),
+        Dog("Ripper", 10, "Poodle"))
+
+    dogs[1].bark()
+    dogs[1].weight = 15
+    println("Weight for ${dogs[1].name} is ${dogs[1].weight}")
 }
